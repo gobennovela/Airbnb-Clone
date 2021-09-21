d5a92b09c7b067217d8e974240775b7f8ef17a27
ben <rampiondml@gmail.com>
2021-09-21 15:29:38 +0100

    font changes

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon} from '@h
 
 function Header() {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-sm py-4 md:px-12 px-12">
+        <header className="sticky top-0 z-50 grid md:grid-cols-3 bg-white shadow-sm py-4 md:px-12 px-8 grid-cols-2">
             {/* Left */}
             <div className="relative flex items-center h-8 cursor-pointer my-auto">
                 <Image src="https://links.papareact.com/qd3" 
@@ -14,14 +14,14 @@ function Header() {
                 objectPosition="left"/>
             </div>
             {/* Middle */}
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-smx">
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-smx hidden md:flex">
                 <input className=" text-sm placeholder-gray-400 text-gray-600 flex-grow pl-4 bg-transparent outline-none" type="text" placeholder="Start your search" />
                 <SearchIcon className="mr-2 hidden md:inline-flex text-white h-8 rounded-full p-2 bg-red-400 cursor-pointer md:mx-2"/>
             </div>
             {/* Right */}
             <div className="flex space-x-4 items-center justify-end text-gray-500">
                 <p className="hidden md:inline cursor-pointer text-sm">Become a host</p>
-                <GlobeAltIcon className="h-6"/>
+                <GlobeAltIcon className="h-8 md:h-6"/>
                 <div className="flex border-2 items-center space-x-4 p-2 rounded-full">
                     <MenuIcon className="h-6 cursor-pointer" />
                     <UserCircleIcon className="h-6 cursor-pointer"/>
